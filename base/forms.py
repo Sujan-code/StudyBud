9c11216ed70791df685cf0ca73f6422cfd6d6a2d
Sujan-code <thapaliyasuj@gmail.com>
2023-04-29 11:20:02 +0545

    first commit

---
+++ base/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from .models import Room
+from django.contrib.auth import get_user_model
+User = get_user_model()
+class MyUserCreationForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class RoomForm(ModelForm):
+    
+    class Meta:
+        model = Room
+        fields = '__all__'
+        exclude = ['host', 'participants']
+
+
+class UserForm(ModelForm):
+
+    class Meta:
+        model = User
+        fields =  '__all__'
